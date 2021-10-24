229fb5a49342f8d3aa2c03b765d8bd21a6555275
deari <5dearw21@solent.ac.uk>
2021-10-24 18:15:04 +0100

    sets eyes for beep using user input

---
+++ basics/input/ascii-art.py
@@ -0,0 +1,16 @@
+# Asks the user for what eyes to give Beep
+eyes = input("Please enter a character for the eye")
+print("      /.\\     \n"
+      "      \\./    \n"
+      "       |      \n"
+      "      / \\   \n"
+      ".----'   '----. \n"
+      f"|    {eyes}   {eyes}    | \n"
+      "|             | \n"
+      "|   -.___.-   | \n"
+      "'-------------' \n")
+
+
+
+
+
