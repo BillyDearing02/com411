c014a8a50ca519604a7ad67cf502d74c44e9034e
deari <5dearw21@solent.ac.uk>
2021-10-24 17:56:41 +0100

    Added code that reads a user input and output

---
+++ basics/input/user_input.py
@@ -0,0 +1,8 @@
+# Asks the user to enter their name
+print("What is your name human?")
+# Stores the user input a variable
+name = input()
+# prints a message containing the users name using a binary operator
+print("It is nice to meet you human " + name + "\n")
+# prints a message containing the users name using an f-string
+print(f"It is nice to meet you {name}.")
