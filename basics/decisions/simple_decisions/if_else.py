b207f1644d5f166da52d3aac9c6bdb3187b32279
deari <5dearw21@solent.ac.uk>
2021-10-28 15:21:54 +0100

    made a if and else statement that uses a variable to output the correct string based on user input

--- basics/decisions/simple_decisions/if_else.py
+++ basics/decisions/simple_decisions/if_else.py
@@ -1,1 +1,14 @@
-activity =
+# Gets user input on what the activity is
+activity = input("Please enter the activity to be performed.\n")
+
+# checks if the activity is calculate
+if activity == "calculate":
+    # makes the perform variable equal calculations
+    perform = "Calculations"
+else:
+    # makes the perform variable equal activity if the if statement condition isnt met
+    perform = "Activity"
+
+# prints output based on the if statement
+print(f"Performing {perform}...\n\n"
+      "Activity completed!")
