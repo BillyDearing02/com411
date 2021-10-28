a28aab6ea7e21a237eaf913ec6ed0cd118244c8a
deari <5dearw21@solent.ac.uk>
2021-10-28 15:11:41 +0100

    Made a simple if statement decision and added comments

---
+++ basics/decisions/simple_decisions/if_.py
@@ -0,0 +1,9 @@
+# gets the users input to be in the simple if statement
+book = input("What type of book is this?\n")
+
+# checks if the condition for the if statement is met
+if book == "adventure":
+    # if its true it outputs this string
+    print("I like adventure books!\n")
+# if its true or false it outputs this string
+print("Finished reading book.")
