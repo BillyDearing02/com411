9d261811636f5690e2c2daed087e2709cd627332
deari <5dearw21@solent.ac.uk>
2021-11-01 20:58:49 +0000

    made a simple for loop with if statements to check for odd or even and then made it count up based on that

---
+++ basics/repetitions/for_loop/range.py
@@ -0,0 +1,27 @@
+# amount input set as an int
+
+amount = int(input("What level of brightness is required?\n"))
+
+print("Adjusting brightness...")
+# defining an int variable
+number = 0
+# Checking if the user input is even
+if amount % 2 == 0:
+    for count in range(0, amount, 2):#
+        # increments by 2 as its even
+        number = number + 2
+        # converts the counter number to an *
+        ans = "*" * number
+        print(f"Beep's brightness level: {ans}")
+        print(f"Beep's brightness level: {ans}\n\n")
+# in case the user input is odd it increments by 1 instead of 2
+else:
+    for count in range(0, amount):
+        # increments by 1 as its odd.
+        number = number + 1
+        # converts the counter number to an *
+        ans = "*" * number
+        print(f"Beep's brightness level: {ans}")
+        print(f"Beep's brightness level: {ans}\n\n")
+
+# if you wanted to make both odd and even increment by 2 you could just define the number variable in the if/else statements. For the even number = 0 and for odd number = 1
