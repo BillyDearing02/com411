47ccd8cbf73251d6ddf39d92b00bc1c23be785ca
deari <5dearw21@solent.ac.uk>
2021-11-01 18:41:34 +0000

    Made a simple while loop that tells the user the factorial of whatever number they enter

--- basics/repetitions/while_loop/factorial.py
+++ basics/repetitions/while_loop/factorial.py
@@ -1,6 +1,9 @@
+# sets the input as an int/getting user input
 number = int(input("Please enter a number.\n"))
+# ans has equal 1 not 0 to be multiplied / defining as int variables
 ans = 1
 factorial = 0
+# loops until number
 while number > 0:
     number = number - 1
     factorial = factorial + 1
