7a82fc2b0c7c5c6461f720931910b3449a8466dc
deari <5dearw21@solent.ac.uk>
2021-11-02 17:33:32 +0000

    Made a simple function that gets called after the user inputs a sound

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,8 @@
+# defined function to print the output
+def listen(s):
+    print(f"That was a loud {s}!")
+
+# gets the user input to be used as
+sound = input("What sound did I hear?")
+# calls the function and uses the sound variable.
+listen(sound)
