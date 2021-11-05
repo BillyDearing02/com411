a335979d2b8d661086e76ba2bc5deeaefb6d326a
5dearw21 <5dearw21@solent.ac.uk>
2021-11-05 11:20:29 +0000

    Used a set and 2 functions to print what is inside the set.

---
+++ basics/data/sets/simple_set.py
@@ -0,0 +1,16 @@
+
+def observed():
+
+    observations = {'Flying Car','Sky Scraper','Laser','Dome'}
+
+    return observations
+
+def run():
+
+    seen = observed()
+    print(seen)
+
+
+run()
+
+
