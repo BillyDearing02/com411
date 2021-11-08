d5b8089224ab03ebb01a2b328a5a75330863d9b0
deari <5dearw21@solent.ac.uk>
2021-11-08 15:26:55 +0000

    use of imports to import other py files

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,7 +1,8 @@
 # Display message to the standard output
-print("System Failure Imminent!\n"
-      "System reboot has been initiated...\n\n"
-      "...rebooting sensory system\n"
-      "...rebooting output motors\n"
-      "...rebooting hover engine\n\n"
-      "System Online.")
+def run():
+      print("System Failure Imminent!\n"
+            "System reboot has been initiated...\n\n"
+            "...rebooting sensory system\n"
+            "...rebooting output motors\n"
+            "...rebooting hover engine\n\n"
+            "System Online.")
